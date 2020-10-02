650a6b5ec182620babbafc8614e7b81b93993416
germanbibel93 <germanbibel93@gmail.com>
2020-10-02 20:19:48 -0300

    first commit

---
+++ src/utils/loadFromAsyncStorange.ts
@@ -0,0 +1,9 @@
+import { AsyncStorage } from "react-native";
+import { Notes } from '../types/index';
+export const loadFromAsyncStorange = async(): Promise<Notes[]> => {
+    const notes = await AsyncStorage.getItem('notes');
+    if (notes && notes.length > 0) {
+        return JSON.parse(notes)
+    }
+    return [];
+}
---
+++ src/utils/updateAsyncStorage.ts
@@ -0,0 +1,20 @@
+import { AsyncStorage } from "react-native";
+import { Notes } from '../types/index';
+
+export const updateAsyncStorage = (notes: Notes[]) => {
+
+    return new Promise( async(resolve, reject) => {
+
+        try {
+
+            await AsyncStorage.removeItem('notes');
+            await AsyncStorage.setItem('notes', JSON.stringify(notes));
+            return resolve(true);
+
+        } catch(e) {
+            return reject(e);
+        }
+
+    });
+
+}
